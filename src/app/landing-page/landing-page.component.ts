bafb97f3a816e149eec70fb932e2e6f4ccdfb2d7
Bhattacharjee, Riya <riybhattacharjee@deloitte.com>
2023-02-19 13:21:19 +0530

    code formatted

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -9,18 +9,25 @@ import {
   NgForm,
   Validators,
 } from '@angular/forms';
-import { HttpClient, HttpEvent, HttpEventType, HttpRequest, HttpResponse } from '@angular/common/http';
-import { DataSource, ResultsPageComponent } from '../results-page/results-page.component';
+import {
+  HttpClient,
+  HttpEvent,
+  HttpEventType,
+  HttpRequest,
+  HttpResponse,
+} from '@angular/common/http';
+import {
+  DataSource,
+  ResultsPageComponent,
+} from '../results-page/results-page.component';
 import { Observable } from 'rxjs/internal/Observable';
-import { NgxSpinnerService } from "ngx-spinner";
-
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.css'],
 })
-
 @Injectable({
   providedIn: 'root',
 })
@@ -28,11 +35,11 @@ export class LandingPageComponent implements OnInit {
   articles: any;
   apiSpec: string = '';
   technology: string = '';
-  upload_file:File | undefined;
+  upload_file: File | undefined;
   fileName = '';
   generateReportClicked: boolean = false;
   selectedFiles?: FileList;
-  selectedFilesJar?:FileList;
+  selectedFilesJar?: FileList;
   //selectedFiles?: File;
   currentFile?: File;
   //currentFile:any;
@@ -40,39 +47,35 @@ export class LandingPageComponent implements OnInit {
   progress = 0;
   message = '';
   fileInfos?: Observable<any>;
-  baseUrl:string='';
-  tech:string='';
-   
+  baseUrl: string = '';
+  tech: string = '';
+
   constructor(
     private router: Router,
     private apiService: ApiServiceService,
-   // private resultPage:ResultsPageComponent,
-    private route:ActivatedRoute,
-    private spinner: NgxSpinnerService,
+    // private resultPage:ResultsPageComponent,
+    private route: ActivatedRoute,
+    private spinner: NgxSpinnerService
   ) {}
 
-  states !: Observable<object>;
+  states!: Observable<object>;
   ngOnInit(): void {
     //this.fileInfos = this.apiService.getFiles();
   }
 
-
   //new code trial
   model: Model = new Model('', '');
-  
-  
+
   @ViewChild('f') form: any;
 
   techs: string[] = ['Open Spec API', 'Jar File', 'GraphQL'];
 
   onSubmit(f: NgForm) {
-   if (this.form.valid) {
+    if (this.form.valid) {
       this.spinner.show();
       //this.form.reset();
     }
-    
   }
-
 
   onFileSelected(event: any) {
     this.selectedFilesJar = event.target.files;
@@ -82,109 +85,30 @@ export class LandingPageComponent implements OnInit {
     this.selectedFiles = event.target.files;
   }
 
-
-
   //trial for api spec
 
   generateReport(f: NgForm): void {
-    
-    localStorage.setItem("technology",f.value['technology'])
+    localStorage.setItem('technology', f.value['technology']);
     this.progress = 0;
-    this.baseUrl=f.value['apiSpec'];
+    this.baseUrl = f.value['apiSpec'];
     if (this.selectedFiles) {
-     // this.spinner.show();
+      // this.spinner.show();
       const file: File | null = this.selectedFiles.item(0);
 
       if (file) {
         this.currentFile = file;
 
-        this.apiService.getResults(this.currentFile,this.baseUrl).subscribe({
+        this.apiService.getResults(this.currentFile, this.baseUrl).subscribe({
           next: (event: any) => {
             if (event.type === HttpEventType.UploadProgress) {
-              this.progress = Math.round(100 * event.loaded / event.total);
+              this.progress = Math.round((100 * event.loaded) / event.total);
             } else if (event instanceof HttpResponse) {
               this.message = event.body.message;
               //this.fileInfos = this.apiService.getFiles();
               this.articles = event.body;
-            this.router.navigate(['app-results-page']);
-            
-            this.apiService.passDatatoResultsPage(this.articles);
-           this.spinner.hide();
-            }
-          },
-          error: (err: any) => {
-            console.log(err);
-            this.progress = 0;
+              this.router.navigate(['app-results-page']);
 
-            if (err.error && err.error.message) {
-              this.message = err.error.message;
-            } else {
-              this.message = 'Could not upload the file!';
-            }
-
-            this.currentFile = undefined;
-          }
-        });
-     }
-     
-
-      this.selectedFiles = undefined;
-    }
-        else{
-      this.spinner.show();
-      console.log("in else")
-     this.apiService.getResultswithoutFile(this.baseUrl).subscribe({
-      next: (event: any) => {
-        if (event.type === HttpEventType.UploadProgress) {
-          this.progress = Math.round(100 * event.loaded / event.total);
-        } else if (event instanceof HttpResponse) {
-          this.message = event.body.message;
-          //this.fileInfos = this.apiService.getFiles();
-          this.articles = event.body;
-        this.router.navigate(['app-results-page']);
-        
-        this.apiService.passDatatoResultsPage(this.articles);
-       this.spinner.hide();
-        }
-      },
-      error: (err: any) => {
-        console.log(err);
-        this.progress = 0;
-
-        if (err.error && err.error.message) {
-          this.message = err.error.message;
-        } else {
-          this.message = 'Could not upload the file!';
-        }
-
-        this.currentFile = undefined;
-      }
-    });
-    }
-  }
-
-
-
-  generateReportIfJar(f: NgForm): void {
-    this.progress = 0;
-    this.baseUrl=f.value['apiSpec'];
-    if (this.selectedFiles &&this.selectedFilesJar) {
-      const file: File | null = this.selectedFiles.item(0); //excel file
-       const fileJar:File|null=this.selectedFilesJar.item(0); //jar file
-      if (file && fileJar) {
-        this.currentFile = file;
-        this.currentJar=fileJar;
-        this.apiService.upload(this.currentFile,this.baseUrl,this.currentJar).subscribe({
-          next: (event: any) => {
-            if (event.type === HttpEventType.UploadProgress) {
-              this.progress = Math.round(100 * event.loaded / event.total);
-            } else if (event instanceof HttpResponse) {
-              this.message = event.body.message;
-              
-              this.articles = event;
-              
-              this.router.navigate(['app-results-page'])
-              this.apiService.passDatatoResultsPage(this.articles['body']);
+              this.apiService.passDatatoResultsPage(this.articles);
               this.spinner.hide();
             }
           },
@@ -199,13 +123,86 @@ export class LandingPageComponent implements OnInit {
             }
 
             this.currentFile = undefined;
-            this.currentJar = undefined;
-          }
+          },
         });
       }
-      this.selectedFilesJar=undefined;
+
+      this.selectedFiles = undefined;
+    } else {
+      this.spinner.show();
+      console.log('in else');
+      this.apiService.getResultswithoutFile(this.baseUrl).subscribe({
+        next: (event: any) => {
+          if (event.type === HttpEventType.UploadProgress) {
+            this.progress = Math.round((100 * event.loaded) / event.total);
+          } else if (event instanceof HttpResponse) {
+            this.message = event.body.message;
+            //this.fileInfos = this.apiService.getFiles();
+            this.articles = event.body;
+            this.router.navigate(['app-results-page']);
+
+            this.apiService.passDatatoResultsPage(this.articles);
+            this.spinner.hide();
+          }
+        },
+        error: (err: any) => {
+          console.log(err);
+          this.progress = 0;
+
+          if (err.error && err.error.message) {
+            this.message = err.error.message;
+          } else {
+            this.message = 'Could not upload the file!';
+          }
+
+          this.currentFile = undefined;
+        },
+      });
+    }
+  }
+
+  generateReportIfJar(f: NgForm): void {
+    this.progress = 0;
+    this.baseUrl = f.value['apiSpec'];
+    if (this.selectedFiles && this.selectedFilesJar) {
+      const file: File | null = this.selectedFiles.item(0); //excel file
+      const fileJar: File | null = this.selectedFilesJar.item(0); //jar file
+      if (file && fileJar) {
+        this.currentFile = file;
+        this.currentJar = fileJar;
+        this.apiService
+          .upload(this.currentFile, this.baseUrl, this.currentJar)
+          .subscribe({
+            next: (event: any) => {
+              if (event.type === HttpEventType.UploadProgress) {
+                this.progress = Math.round((100 * event.loaded) / event.total);
+              } else if (event instanceof HttpResponse) {
+                this.message = event.body.message;
+
+                this.articles = event;
+
+                this.router.navigate(['app-results-page']);
+                this.apiService.passDatatoResultsPage(this.articles['body']);
+                this.spinner.hide();
+              }
+            },
+            error: (err: any) => {
+              console.log(err);
+              this.progress = 0;
+
+              if (err.error && err.error.message) {
+                this.message = err.error.message;
+              } else {
+                this.message = 'Could not upload the file!';
+              }
+
+              this.currentFile = undefined;
+              this.currentJar = undefined;
+            },
+          });
+      }
+      this.selectedFilesJar = undefined;
       this.selectedFiles = undefined;
     }
   }
-  
 }
--- src/app/results-page/results-page.component.ts
+++ src/app/results-page/results-page.component.ts
@@ -7,174 +7,175 @@ import { AgGridAngular } from 'ag-grid-angular';
 import { CellClickedEvent, ColDef, GridReadyEvent } from 'ag-grid-community';
 import { NgxSpinnerService } from 'ngx-spinner';
 
-
 export interface DataSource {
   className: string;
   method: string;
   baseUrl: string;
-  path:string;
-  payloadJson:string;
-  responseTime:string;
-  expectedStatus:string;
-  responseStatus:string;
-  passedOrFailed:string;
-  pathParam:string;
-  requestParam:string;
-  responsePayload:string;
+  path: string;
+  payloadJson: string;
+  responseTime: string;
+  expectedStatus: string;
+  responseStatus: string;
+  passedOrFailed: string;
+  pathParam: string;
+  requestParam: string;
+  responsePayload: string;
 }
 
 @Component({
   selector: 'app-results-page',
   templateUrl: './results-page.component.html',
-  styleUrls: ['./results-page.component.css']
+  styleUrls: ['./results-page.component.css'],
 })
 @Injectable({
   providedIn: 'root',
 })
-
-
 export class ResultsPageComponent implements OnInit {
-  
-  
-
-goBack(){
+  goBack() {
     this.router.navigate(['app-results-page']);
     this.apiService.passDatatoResultsPage(this.lastRowData);
   }
 
-// Each Column Definition results in one Column.
-public columnDefs: ColDef[] = [
-  { field: 'className', checkboxSelection: true, headerCheckboxSelection: true },
-  { field: 'method' },
-  { field: 'baseUrl' },
-  { field: 'path' },
-  { field: 'payloadJson' },
-  { field: 'pathParam' },
-  { field: 'requestParam' },
-  { field: 'responseTime' },
-  { field: 'expectedStatus' },
-  { field: 'responseStatus' },
-  { field: 'passedOrFailed' },
-  { field: 'responsePayload'},
-];
+  // Each Column Definition results in one Column.
+  public columnDefs: ColDef[] = [
+    {
+      field: 'className',
+      checkboxSelection: true,
+      headerCheckboxSelection: true,
+    },
+    { field: 'method' },
+    { field: 'baseUrl' },
+    { field: 'path' },
+    { field: 'payloadJson' },
+    { field: 'pathParam' },
+    { field: 'requestParam' },
+    { field: 'responseTime' },
+    { field: 'expectedStatus' },
+    { field: 'responseStatus' },
+    { field: 'passedOrFailed' },
+    { field: 'responsePayload' },
+  ];
 
-public columnDefsforGraphQl:ColDef[]=[
-  { field: 'apiName', checkboxSelection: true, headerCheckboxSelection: true },
-  { field: 'payloadJson' },
-  { field: 'pathParam' },
-  { field: 'passedOrFailed' },
-  { field: 'responsePayload'},
-  { field: 'expectedStatus' },
-  { field: 'responseStatus' },
-  { field: 'responseTime' },
-  
-]
+  public columnDefsforGraphQl: ColDef[] = [
+    {
+      field: 'apiName',
+      checkboxSelection: true,
+      headerCheckboxSelection: true,
+    },
+    { field: 'payloadJson' },
+    { field: 'pathParam' },
+    { field: 'passedOrFailed' },
+    { field: 'responsePayload' },
+    { field: 'expectedStatus' },
+    { field: 'responseStatus' },
+    { field: 'responseTime' },
+  ];
 
-// DefaultColDef sets props common to all Columns
-public defaultColDef: ColDef = {
-  editable: true,
-  sortable: true,
-  filter: true,
-};
+  // DefaultColDef sets props common to all Columns
+  public defaultColDef: ColDef = {
+    editable: true,
+    sortable: true,
+    filter: true,
+  };
 
-public selectedRowstoSend: Array<any> =[];
-// Data that gets displayed in the grid
-public rowData = <any>[];
-public lastRowData =<any>[];
+  public selectedRowstoSend: Array<any> = [];
+  // Data that gets displayed in the grid
+  public rowData = <any>[];
+  public lastRowData = <any>[];
 
-// For accessing the Grid's API
-@ViewChild(AgGridAngular) agGrid!: AgGridAngular;
+  // For accessing the Grid's API
+  @ViewChild(AgGridAngular) agGrid!: AgGridAngular;
 
+  progress = 0;
+  message = '';
+  articles: any;
+  count = 0;
+  nodes: any;
+  technology: any;
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    private apiService: ApiServiceService,
+    private spinner: NgxSpinnerService
+  ) {}
 
-progress = 0;
-message = '';
-articles:any;
-count=0;
- nodes:any;
- technology:any
-constructor(private http: HttpClient,private router: Router,private apiService: ApiServiceService,private spinner: NgxSpinnerService,) {}
-
-
-onGridReady(params:GridReadyEvent){
- this.technology= localStorage.getItem("technology")
-this.apiService.content.subscribe((data)=>{
-  this.rowData = data;
-})
-}
-possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890,./;'[]\=-)(*&^%$#@!~`";
-
-// Example of consuming Grid Event
-onCellClicked(e: CellClickedEvent): void {
-}
-
-// Example using Grid's API
-duplicateRowsSelected(): void {
-
-  let text = "";
-  for (let i = 0; i < 7; i++) {
-    text += this.possible.charAt(Math.floor(Math.random() * this.possible.length));
-  }
-
-  const nodes = this.agGrid.api.getSelectedNodes();
-  //let count=0;
-  
-  for(let node  of nodes){
-    const data = JSON.parse(JSON.stringify(node['data']))
-    let splitString
-    if(data['className'].split('_')){
-      splitString=data['className'].split('_')[0];
-      data['className'] = splitString+'_dup_'+text
-    }
-    else{
-      data['className'] = data['className']+'_dup_'+text
-    }
-   // data['className'] = data['className']+'_dup_'+text
-    if(node.rowIndex != null){
-      this.rowData.splice(node.rowIndex + this.count, 0, data)
-      this.agGrid.api.setRowData(this.rowData); 
-      this.lastRowData=this.rowData;
-      console.log(this.rowData)
-    }
-  }
-}
-
-ngOnInit(): void {
-    
-}
-
-
-regenerateReport(){
-  this.spinner.show()
-  this.progress = 0;
-  var i;
-  console.log(this.agGrid.api.getSelectedNodes())
-  if(this.agGrid.api.getSelectedNodes().length=0 && this.agGrid.api.getSelectedNodes()==[]){
-    console.log("inside if")
-    this.apiService.content.subscribe((data)=>{
+  onGridReady(params: GridReadyEvent) {
+    this.technology = localStorage.getItem('technology');
+    this.apiService.content.subscribe((data) => {
       this.rowData = data;
-    })
-    this.nodes=this.rowData
-    
+    });
   }
-  else{
-     this.nodes = this.agGrid.api.getSelectedNodes();
+  possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890,./;'[]=-)(*&^%$#@!~`";
+
+  // Example of consuming Grid Event
+  onCellClicked(e: CellClickedEvent): void {}
+
+  // Example using Grid's API
+  duplicateRowsSelected(): void {
+    let text = '';
+    for (let i = 0; i < 7; i++) {
+      text += this.possible.charAt(
+        Math.floor(Math.random() * this.possible.length)
+      );
+    }
+
+    const nodes = this.agGrid.api.getSelectedNodes();
+    //let count=0;
+
+    for (let node of nodes) {
+      const data = JSON.parse(JSON.stringify(node['data']));
+      let splitString;
+      if (data['className'].split('_')) {
+        splitString = data['className'].split('_')[0];
+        data['className'] = splitString + '_dup_' + text;
+      } else {
+        data['className'] = data['className'] + '_dup_' + text;
+      }
+      // data['className'] = data['className']+'_dup_'+text
+      if (node.rowIndex != null) {
+        this.rowData.splice(node.rowIndex + this.count, 0, data);
+        this.agGrid.api.setRowData(this.rowData);
+        this.lastRowData = this.rowData;
+        console.log(this.rowData);
+      }
+    }
   }
-  console.log(this.nodes)
-  //const nodes = this.agGrid.api.getSelectedNodes();
- // let count=0;
-  for(let node  of this.nodes){
-    const data = JSON.parse(JSON.stringify(node['data']))
-    this.selectedRowstoSend.push(data)
-  }
+
+  ngOnInit(): void {}
+
+  regenerateReport() {
+    this.spinner.show();
+    this.progress = 0;
+    var i;
+    console.log(this.agGrid.api.getSelectedNodes());
+    if (
+      (this.agGrid.api.getSelectedNodes().length =
+        0 && this.agGrid.api.getSelectedNodes() == [])
+    ) {
+      console.log('inside if');
+      this.apiService.content.subscribe((data) => {
+        this.rowData = data;
+      });
+      this.nodes = this.rowData;
+    } else {
+      this.nodes = this.agGrid.api.getSelectedNodes();
+    }
+    console.log(this.nodes);
+    //const nodes = this.agGrid.api.getSelectedNodes();
+    // let count=0;
+    for (let node of this.nodes) {
+      const data = JSON.parse(JSON.stringify(node['data']));
+      this.selectedRowstoSend.push(data);
+    }
     this.apiService.resendSelectedItems(this.selectedRowstoSend).subscribe({
       next: (event: any) => {
         if (event.type === HttpEventType.UploadProgress) {
-          this.progress = Math.round(100 * event.loaded / event.total);
+          this.progress = Math.round((100 * event.loaded) / event.total);
         } else if (event instanceof HttpResponse) {
           this.message = event.body.message;
           this.articles = event;
-          this.rowData=this.articles['body'];
-          this.router.navigate(['app-results-page'])
+          this.rowData = this.articles['body'];
+          this.router.navigate(['app-results-page']);
           this.apiService.passDatatoResultsPage(this.articles['body']);
           this.spinner.hide();
         }
@@ -188,14 +189,8 @@ regenerateReport(){
         } else {
           this.message = 'Could not upload the file!';
         }
-      }
-});
-    this.selectedRowstoSend=[]; 
+      },
+    });
+    this.selectedRowstoSend = [];
+  }
 }
-}
-
-
-
-
-
-
